993199159bbf5411de89b6b478b0973c9bbbb650
rhsaimon <robiulhasan007007@gmail.com>
2026-02-17 22:51:25 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,91 @@
+// console.log("Jerney of JAVA Script");
+// Chapter 1: variable , object, data type..
+
+// Variable:..........
+/*
+// string...
+let name = "Robiul hasan saimon";
+// integer... 
+let age = 21; 
+// floating...
+let A = 44.44;
+// boolean...
+let B = true / false;
+console.log("age: "+age);
+console.log(name);
+console.log(A);
+console.log(B);
+
+
+// Object:......
+
+let student =
+{
+    name: "Saimon",
+    age: 21,
+    cgpa: 3.50,
+    marks: 99.99
+
+};
+console.log(student);
+student["name"] = "Robiul Hasan";
+console.log(student["name"]);
+
+
+// practice question:
+// Q1. Create a const object called "product" to store information shown in the Picture.
+
+const product = {
+    brandname : "Ct Ball Pen",
+    rating : 5,
+    isDeal : true,
+    price : 270,
+    discount : 5,
+};
+
+console.log(product);
+console.log(typeof product);
+
+// Q2. Create a const object called ""profile"" to store info...
+
+const profile ={
+    username : "@rhsaimon",
+    isFallow : true,
+    isMessage : true,
+    post : 22,
+    followers : 350,
+    following : 50,
+    bio : "gentle man"
+};
+
+console.log(profile);
+
+// chapter 2: operators and conditional statements..
+
+// operators:
+
+// arithmetic operators,,,
+let a = 8;
+let b = 6;
+
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b); 
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b); 
+//modulus..
+console.log("a % b = ", a % b); 
+//exponentiation...
+console.log("a ** b = ", a ** b);
+
+// Assignment operators:(+=, -=, *=, /=)
+
+let a = 5;
+let b = 4;
+
+a += 4;
+b -= 2;
+console.log("a =", a);
+console.log(b);
+*/
+
+// Comparison operators:(!=, ==)
