5a623cf0e008e73894031f7a67aeeb2de0c4a81d
RH-Saimon <robiul.h03@uttarauniversity.edu.bd>
2026-02-18 10:49:42 +0600

    added

---
+++ conditionalstatement.js
@@ -0,0 +1,32 @@
+//Conditional statement: (if, else-if, else)
+/*
+let age = 21;
+if(age >= 18){
+    console.log("you can vote.");
+}
+else{
+    console.log("you cant vote");
+}
+
+let mode = "dark";
+let color;
+if(mode === "dark"){
+    color = "black";
+}
+if(mode === "white"){
+    color = "white";
+}
+console.log(color);
+
+
+// chek even or odd.....
+
+let n = 21;
+
+if(n % 2 === 0){
+    console.log("this is even");
+}
+else{
+    console.log("this is odd");
+}
+*/
