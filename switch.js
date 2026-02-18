b4c57ac5ff81b6c73c30c3d1b89cdd5aec2d3fdf
RH-Saimon <robiul.h03@uttarauniversity.edu.bd>
2026-02-18 11:32:30 +0600

    add

---
+++ switch.js
@@ -0,0 +1,15 @@
+let fruit = "mango";
+switch (fruit){
+    case "apple":
+        console.log("this is an apple");
+    
+    case "banana":
+        console.log("this is a banana");
+    
+    case "mango":
+        console.log("this is a mango");
+    break;
+    default:
+        console.log("sry, we are not found the fruit");    
+
+}
---
+++ ternary.js
@@ -0,0 +1,8 @@
+// ternary operators:
+
+let age = 21;
+
+// age >= 18 ? console.log("adult") : console.log("not adult");
+
+let result = age >= 18 ? "adult" : "not adult";
+console.log(result);
