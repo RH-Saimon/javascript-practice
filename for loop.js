7531b129781b0ec373a707a18d671fe65c6d51aa
RH-Saimon <robiul.h03@uttarauniversity.edu.bd>
2026-02-20 12:24:56 +0600

    for, for..of, for...in

---
+++ for loop.js
@@ -0,0 +1,46 @@
+// for loop = repeat something specific number of times
+/*
+// print 1 to 5...
+for(let i = 1; i <= 50; i++){
+    console.log("for loop")
+}
+
+// calcutale sum of 1 to 5?
+let sum = 0;
+for(let i = 1; i <= 5; i++){
+    sum += i;
+}
+console.log(sum);
+
+
+// for-of loop: works with value, better for array and string. 
+
+let char = prompt("enter your char:"); // for user input
+let lenth = 0;
+for (let i of char){
+    console.log(i);
+    lenth++;
+}
+console.log("lenth = ", lenth);
+
+
+let numbers = [10, 20, 30];  //array for number 
+
+for (let value of numbers) {
+   console.log(value);
+}
+
+
+// For in loop: works with index/key, better for object.
+
+let student ={
+    name : "saimon",
+    age : 21,          // }object
+    cgpa : 3.34,
+    isPass : true
+}
+for (let i in student){
+    console.log(i+":", student[i]); //i for key and student[i] for key value.
+}
+
+*/
