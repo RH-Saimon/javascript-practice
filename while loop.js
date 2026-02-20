fd84498007fd563757866fe2bc4bca0f743a3664
RH-Saimon <robiul.h03@uttarauniversity.edu.bd>
2026-02-20 12:25:41 +0600

    while, do..while

---
+++ while loop.js
@@ -0,0 +1,18 @@
+// when i don't remember how many time iterate in program loop;
+
+let i = 1;
+while(i <= 5){
+    console.log(i);
+    i++;
+}
+
+// Do-while loop;
+
+let i = 1;
+do{
+    console.log("sam");
+    i++;
+}
+while(i <= 10);
+
+
