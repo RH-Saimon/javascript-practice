8c3dada3d70417caa38856201f409fcc4d15c6d5
RH-Saimon <robiul.h03@uttarauniversity.edu.bd>
2026-02-22 01:33:31 +0600

    add

---
+++ arrMethod.js
@@ -0,0 +1,25 @@
+// Method Push : items.push("something") = it's add item in the end of it
+//even we add mutiple value or item.
+/*
+let item = ["ti", "ri", "ei", "ji"];
+item.push("bi");  //add bi at the end of ji.
+console.log(item);
+
+
+//Method Pop : item.pop("something") = it's delete item in the end of item array.
+// there is only one item delete at the end.
+
+let item = ["ti", "ri", "ei", "ji"];
+item.pop();
+console.log(item);
+*/
+
+// Method toString : item.toString() = it's convert all array elements into string type.
+
+let item = ["ti", "ri", "ei", "ji"];
+console.log(item.toString());
+
+let marks = [3, 5, 6, 7];
+console.log(marks.toString());
+
+
