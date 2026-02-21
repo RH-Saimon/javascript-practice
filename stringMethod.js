12d7656d034ffd170cfa101276f5abfa12d64d61
RH-Saimon <robiul.h03@uttarauniversity.edu.bd>
2026-02-21 15:48:34 +0600

    with practice q1

---
+++ stringMethod.js
@@ -0,0 +1,42 @@
+//For uppercase : str.toUpperCase()
+//For lowercase : str.toLowerCase()
+//But old string are not change , its create new variable and change it to upper/lower.
+//For Ex:
+/*
+let str = "Robiul hasan saimon";
+console.log(str.toUpperCase());  
+console.log(str.toLowerCase());
+
+//str.trim() : use for remove whitespace means before and after space of a string.
+// For Ex:
+let string = "      robiul  hasan saimon        ";
+console.log(string.trim());  //output: (robiul  hasan saimon)
+
+
+//For slicing : string.slice(start index, end index)
+let string = "saimonkhan";
+console.log(string.slice(1,6));
+
+
+//For adding to string : string1.concat(string2) / (string1 + string2)
+let string1 = "sai";
+let string2 = "mon";
+console.log(string1.concat(string2));  //saimon
+
+
+//For replacing any value : string.replace(searchVal, newVal)
+// let str = "saimon";
+// console.log(str.replace('mon', 'p'));  //saip
+
+
+//For search any single character : string.charAt()
+let str = "saimon";
+console.log(str.charAt(3));  // (m)
+
+//practice Q1: enter from user input fulname (saimonkhan) it should be come (@saimonkhan10), 10 means name length.
+
+let username = prompt("enter your fullname with no space:");
+
+// let namelength = username.length;
+console.log(`@${username}${username.length}`); 
+*/
