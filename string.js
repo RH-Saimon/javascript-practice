88964698f71b3e66abed620293d5609b5617cae1
RH-Saimon <robiul.h03@uttarauniversity.edu.bd>
2026-02-21 14:55:38 +0600

    add

---
+++ string.js
@@ -0,0 +1,26 @@
+//string:
+/*
+let str = "saimon";
+console.log(str.length); //length print
+console.log(str[0]);   //single index letter print 
+
+
+// Template literals: A way to have embedded expression in string.
+
+let string = `this is the template literals.`;
+let string = `this is the template literals ${2+2+3}`;
+console.log(string);
+
+// for ex:
+let product = {
+    brandname : "Ct Ball Pen",
+    rating : 5,
+    isDeal : true,
+    price : 270,
+    discount : 5,
+};
+console.log(`brand name ${product.brandname}, rating ${product.rating}, price ${product.price}`);
+
+
+// Scape characters: \n for new line, \t for tap/space. 
+*/
